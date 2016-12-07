830244f1ff0f956ace073a59088fcdec1dd7507c
Ethan Steiner <esteiner@nuxeo.com>
2016-12-07 16:52:02 -0500

    task-NXP-21049: edit of functional test delay time

--- polymer-nuxeo-mp/ftest/nightwatch/tests/smoke_test.js
+++ polymer-nuxeo-mp/ftest/nightwatch/tests/smoke_test.js
@@ -6,7 +6,7 @@ module.exports = {
             .setValue('@username', 'Administrator')
             .setValue('@password', 'Administrator')
             .click('@submit')
-            .waitForElementVisible('@myApp', 5000)
+            .waitForElementVisible('@myApp', 8000)
             .waitForElementVisible('@appLocation', 1000)
             .waitForElementVisible('@appHeader', 1000)
             .waitForElementVisible('@rootFolder', 1000)
@@ -17,16 +17,16 @@ module.exports = {
         polymerUi.navigate()
             .waitForElementVisible('@addFolder', 1000)
             .click('@addFolder')
-            .waitForElementVisible('@folderAddModal', 2000)
+            .waitForElementVisible('@folderAddModal', 5000)
             .assert.containsText('@folderAddModal', 'New Folder')
             .click('@closeFolderAddModal')
     },
     'Polymer UI Smoke Test - Add Document Test' : function (browser) {
         var polymerUi = browser.page.polymerui();
         polymerUi.navigate()
-            .waitForElementVisible('@addDocument', 1000)
+            .waitForElementVisible('@addDocument', 5000)
             .click('@addDocument')
-            .waitForElementVisible('@documentAddModal', 2000)
+            .waitForElementVisible('@documentAddModal', 5000)
             .assert.containsText('@documentAddModal', 'New Document')
             .click('@closeDocumentAddModal');
         browser.end();
